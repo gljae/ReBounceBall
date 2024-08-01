5f32ae73c21209b9c0d8480824b2727075a4d182
gijae05 <91142899+gljae@users.noreply.github.com>
2024-08-01 17:19:46 +0900

    ReBounceBall0.1.3

--- ReBounceBall/Assets/Script/PlayerBall.cs
+++ ReBounceBall/Assets/Script/PlayerBall.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerBall : MonoBehaviour
 {
+    public static int score;
     public float tileJump;
+    public float DeviceJump;
     public float moveSpeed;
     public float maxSpeed;
     private Rigidbody2D rb;
     void Start()
     {
+        score = 0;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -19,11 +24,11 @@ public class PlayerBall : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(Vector2.left * moveSpeed, ForceMode2D.Force);       
+            rb.AddForce(Vector2.left * moveSpeed * Time.deltaTime * 60, ForceMode2D.Force);       
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Force);
+            rb.AddForce(Vector2.right * moveSpeed * Time.deltaTime * 60, ForceMode2D.Force);
         }
         if (rb.velocity.x < maxSpeed * (-1))
             rb.velocity = new Vector2(maxSpeed * (-1), rb.velocity.y);
@@ -34,7 +39,7 @@ public class PlayerBall : MonoBehaviour
 
     public void Jump(float jumpForce)
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * jumpForce * Time.deltaTime * 60, ForceMode2D.Impulse);
         Debug.Log("jump");
     }
    
@@ -47,6 +52,29 @@ public class PlayerBall : MonoBehaviour
             Jump(tileJump);
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "SawBlade")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (collision.transform.tag == "JumpDevice")
+        {
+            Jump(DeviceJump);
+            collision.gameObject.GetComponent<Animator>().SetTrigger("Jump");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Strawberry")
+        {
+            score += 1;
+            Debug.Log(score);
+            collision.gameObject.SetActive(false);
+        }
     }
 
 
